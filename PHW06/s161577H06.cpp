f26b3295aa5731a1f4e9799d959a6b3a96d21761
hummingbird-12 <entropyih@sogang.ac.kr>
2020-11-23 02:03:47 +0900

    PHW06 - Initial files

---
+++ PHW06/s161577H06.cpp
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <queue>
+
+#define NONE -1
+
+typedef struct elm_edge {
+	int  vf, vr;  // e = (vf, vr) where vf, vr are vertex array indices
+	int  cost;    // edge cost
+	bool flag;    // true if the edge is in the SP spanning tree, false otherwise 
+	int  fp, rp;  // adj list ptr of forward and reverse (-1 if none)
+} edge;
+
+typedef struct elm_vertex {
+	int  f_hd, r_hd;	// adj list header(-1 if none)
+	int  distance;		// distance from src to this vertex
+	int  heap_idx;		// minHeap index
+	bool inS;			// true if SP length found(true when move from V-S to S) 
+} vertex;
+
+int SPT_Dijkstra(
+	int src,	// source vertex index
+	// graph structure array
+	// 1. the adjacency list structure is the same as PHW02
+	// 2. additional fields are added for Dijkstra's algorithm(see .h file)
+	int Vnum, vertex *V,	// Vertex array size and the array
+	int Enum, edge *E,		// Edge array size and the array
+
+	int *minHeap	// array for min heap (array size = Vnum+1)
+		// heap index range is 1 ~ (Vnum - 1) note: src must not in the initial heap
+		// just arry is passed. must construct min-heap in this function
+
+	// OUTPUT
+	// 1. V[].distance : shortest path length from src to this vertex
+	// 2. shortest path spanning tree : set E[].flag = true if the edge is in the tree
+	// 3. return the sum of edge costs in the shortest path spanning tree.
+	//    should be small as possible (think a way to make it small)
+) {
+	int treeCost = 0;
+	// *** 이 함수를 작성하자 ***
+	// 반드시 min-heap을 사용하여 O((n+m)logn) 알고리즘을 구현해야 한다(아니면 trivial한 프로그램임)
+	// heap 연산 등 필요한 함수는 자유롭게 작성하여 추가한다.
+	// 그러나 global 변수, dynamic array 등은 추가로 사용하지 않는다(실제로 필요 없다)
+	
+
+
+	return treeCost;
+}
+
+void Read_Graph(int Vnum, vertex *V, int Enum, edge *E) {
+	// Graph 자료구조를 만드는 함수
+	// *** 이 함수를 추가하자 ***
+	// PHW02의 Read_Graph_adj_array()를 이 과제의 자료구조에 맞춰 살짝 수정하여 사용한다
+	// 즉, Read_Graph_adj_array()에서 불필요한 내용을 제거한 후 사용.
+
+}
+
+// the following functions are for testing if the submitted program is correct.
+int  Tree_Check(int Vnum, vertex *V, int Enum, edge *E, int *visited);
+bool SPT_test(int src, int Vnum, vertex *V, int Enum, edge *E, int *minHeap);
+void Error_Exit(const char *s);
+
+int main ( void ) {
+	int		src;
+	vertex *V;		int Vnum;
+	edge   *E;		int Enum;
+	int    *minHeap;
+	int    Tree_cost;
+	int    Tnum; 		// # of test cases
+	clock_t start, finish;
+	double cmpt;
+
+	scanf_s("%d", &Tnum);		// read # of tests
+
+	for (int t = 0; t < Tnum; t++ ) {
+		scanf_s("%d %d %d", &Vnum, &Enum, &src);
+		V = new vertex [Vnum];
+		E = new edge [Enum];
+		minHeap = new int[Vnum + 1];	// heap array allocation
+		if ( V == NULL || E == NULL || minHeap == NULL ) {
+			Error_Exit("Memory Allocation Error");
+		}
+		Read_Graph(Vnum, V, Enum, E);
+
+		/**/start = clock();	// start timer
+
+		Tree_cost = SPT_Dijkstra(src, Vnum, V, Enum, E, minHeap);	// code by students
+
+		/**/finish = clock();	// stop timer
+		cmpt = ((double)(finish - start)) / (double)CLK_TCK;
+
+		// 아래 Tree_Check와 SPT_test 함수는 첨부한 SPT_test.obj 파일에 있다.
+		// 이 테스트에서 오류로 인하여 프로그램이 정지하면 뭔가 잘못된 것이다(제출해도 0점)
+        if (Tree_Check(Vnum, V, Enum, E, minHeap) == 0) {
+			Error_Exit("   ERROR The result is not a spanning tree");
+		}
+		if (SPT_test(src, Vnum, V, Enum, E, minHeap) == false) {
+			Error_Exit("** Something wrong in applying Dijkstra's");
+		}
+		if ( t != 0 ) 
+			printf("\n");
+
+		printf("**T%d (Dijkstra) (V = %d, E = %d, time = %.3f sec) Tree Cost = %d\n", 
+			t+1, Vnum, Enum, cmpt, Tree_cost);
+
+		delete [] minHeap; delete [] V; delete [] E;
+	}
+	return 0;
+}
+
+void Error_Exit (const char *s ) {
+  printf("%s\n", s);
+  exit(-1);
+}
